ab944a4ee850aa5c9f7c03ad0204f163a23dd158
mstva <mstva@gmail.com>
2021-01-12 22:17:25 +0200

    generate the artist and record scaffold and migrate them to database

---
+++ db/migrate/20210112201548_create_records.rb
@@ -0,0 +1,11 @@
+class CreateRecords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :records do |t|
+      t.string :title
+      t.references :user, null: false, foreign_key: true
+      t.references :artist, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_200343) do
+ActiveRecord::Schema.define(version: 2021_01_12_201548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_artists_on_user_id"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.bigint "artist_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_records_on_artist_id"
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -22,4 +40,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_200343) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "artists", "users"
+  add_foreign_key "records", "artists"
+  add_foreign_key "records", "users"
 end
